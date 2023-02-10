7c963bf77d1017516c30e35672980ce13e0de2ac
vspaz <vladimir.edelman@gmail.com>
2023-02-10 20:47:50 +0100

    add url mapping

---
+++ src/ork8r/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views.service import health
+
+urlpatterns = [
+    path("ping/", health.PingView.as_view()),
+]
---
+++ src/ork8r/views/service/health.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class PingView(APIView):
+
+    def get(self, request):
+        return Response(
+            data={
+                "ping": "pong"
+            }
+        )
