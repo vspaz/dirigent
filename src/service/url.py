0b66a68e1f636996bd0428b83f6ea635e640a259
vspaz <vladimir.edelman@gmail.com>
2023-02-15 19:05:54 +0100

    add HealthProbeView

---
+++ src/service/url.py
@@ -0,0 +1,16 @@
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class HealthProbeView(APIView):
+
+    renderer_classes = [JSONRenderer, ]
+    payload = {'ping': 'pong'}
+
+    def get(self, request, *args, **kwargs):
+        return Response(data=self.payload, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        return Response(data=self.payload, status=status.HTTP_202_ACCEPTED)
