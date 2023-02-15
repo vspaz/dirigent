0f02bc7d896294bea2a8825add6532ccdaae67fa
vspaz <vladimir.edelman@gmail.com>
2023-02-15 19:15:45 +0100

    upate settings with new apps

--- src/config/settings.py
+++ src/config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'service.apps.ServiceConfig',
+    'ork8r.apps.OrchestratorConfig',
 ]
 
 MIDDLEWARE = [
--- src/config/urls.py
+++ src/config/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('api/v1/', include('ork8r.urls')),
+
     path('admin/', admin.site.urls),
-    path('', include('ork8r.urls')),
+    path('', include('service.url')),
+
 ]
